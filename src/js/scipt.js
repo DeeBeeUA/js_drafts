e45739a163c906e9daa305d118f5beef5766a570
Bilov D. <deebeekiev@gmail.com>
2024-05-01 17:36:53 +0300

    add three blue square

--- src/js/scipt.js
+++ src/js/scipt.js
@@ -1,19 +1,5 @@
 'use strict';
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-let lastMovieWatched = +prompt("Один из последних фильмов?");
-let ratingOfThisFilm = prompt("На сколько оцените его?");
-
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privet:false
-};
-
-personalMovieDB.movies[lastMovieWatched] = ratingOfThisFilm;
-
-console.log(personalMovieDB['movies']);
-
+for (let i = 1; i < 5; i++) {
+   console.log(i);
+}
